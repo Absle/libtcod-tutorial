3e806baaaf9492ecaca1f3778a6f11dee7003062
Anderson Kagel <anderson.kagel@gmail.com>
2019-08-23 17:32:35 -0400

    removed pointless comments from Entity.hpp

--- src/libtuto/include/Entity.hpp
+++ src/libtuto/include/Entity.hpp
@@ -19,10 +19,6 @@ namespace Satk
             const TCODColor &color;
             
             void move(int dx, int dy);
-            //int get_x(){return x;};
-            //int get_y(){return y;};
-            //char get_symbol(){return symbol;}
-            //TCODColor get_color(){return color;}
 
             private:
             int _x, _y;
